41627be07727ace5ddabe9f0547478a62393a078
jeonga214 <jeonga020214@gmail.com>
2023-09-20 14:21:47 +0900

    dfsfsf

--- index.js
+++ index.js
@@ -4,51 +4,62 @@ const fs = require('fs');
 const bodyParser = require('body-parser')
 const app = express();
 
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const data={
-  select:function(){
-    return  JSON.parse(fs.readFileSync('./test.json'));
-    //parse는 json에서 여기로 가져올때``빼서가져와야되니까 넣어주는거
+const data = {
+  select: function () {
+    return JSON.parse(fs.readFileSync('./test.json'));
   },
-  insert:function(newObj){
+  insert: function (newObj) {
     const jsonData = data.select();
-    let newData= [...jsonData, {id:jsonData.length+1, ...newObj}];
-    fs.writeFileSync('./test.json', JSON.stringify(newData) );
+    let newData = [...jsonData, { id: jsonData.length + 1, ...newObj }];
+    fs.writeFileSync('./test.json', JSON.stringify(newData));
     return newData;
   },
-  update:function(){},
-  delete:function(){}
+  update: function () { },
+  delete: function () { }
 }
 
 app.get('/abc', function (req, res) {
-  res.send(  data.select()  )
+  res.send(data.select());
 });
 
 app.delete('/abc/:id', function (req, res) {
   const jsonData = data.select();
-  
-  const {id} = req.params;
-  const delData = jsonData.filter( n=>n.id != id )
+  const { id } = req.params;
 
-   // 업데이트된 데이터의 id 값을 조정 (선택적)
-   for (let i = 0; i < delData.length; i++) {
-    if (delData[i].id >= id) {
-      delData[i].id--;
-    }
+  // 삭제할 항목을 찾음
+  const indexToDelete = jsonData.findIndex(item => item.id == id);
+
+  if (indexToDelete === -1) {
+    // 삭제할 항목이 없으면 오류 응답을 보냄
+    return res.status(404).json({ error: 'Item not found' });
+  }
+
+  // 삭제할 항목을 배열에서 제거
+  jsonData.splice(indexToDelete, 1);
+
+  if (jsonData.length === 0) {
+    // 남은 항목이 1개일 경우, 빈 배열을 반환하고 JSON 파일 초기화
+    fs.writeFileSync('./test.json', JSON.stringify([]));
+    return res.json([]);
+  }
+
+  // ID 값을 재조정
+  for (let i = indexToDelete; i < jsonData.length; i++) {
+    jsonData[i].id--;
   }
 
   // JSON 파일 업데이트
-  fs.writeFileSync('./test.json', JSON.stringify(delData) );
-  //stringify는 여기서 json으로 보낼때``넣어서 보내야되니까 넣어주는거
-  res.send( delData )
+  fs.writeFileSync('./test.json', JSON.stringify(jsonData));
+
+  // 클라이언트로 업데이트된 데이터를 응답
+  res.json(jsonData);
 });
 
 app.post('/insert', function (req, res) {
-  
   res.send(data.insert(req.body));
 });
 
